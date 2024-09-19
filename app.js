f8bc93b094754510264626aaf8a8a8ed04cc83d7
AbdulazizMurad <abdulaziz.murad2001@gmail.com>
2024-09-19 12:38:28 +0300

    middle ware part1

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
+const postsRoutes = require("./api/posts/posts.routes");
+const connectDb = require("./database");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const notFoundHandler = require("./middleWares/notFoundHandler");
+dotenv.config();
+const PORT = process.env.PORT;
 
 connectDb();
+//midlewares:
 app.use(express.json());
-app.use('/posts', postsRoutes);
+app.use(morgan("dev"));
+app.use(cors());
 
-app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+//routes
+app.use("/posts", postsRoutes);
+//middle wares that handles error in the route (path)
+app.use(notFoundHandler);
+//server
+app.listen(PORT, () => {
+  console.log("The application is running on localhost:8000");
 });
---
+++ middleWares/errorHandler.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+  try {
+    return res
+      .status(err.status || 500)
+      .json({ error: err || "Internal Server Error" });
+  } catch (error) {
+    next(error);
+  }
+};
